1adc6f03f01489bdf287abb2e5b0e9c086d187ec
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-10 15:18:29 +0000

    コントローラー修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,6 @@ class MoviesController < ApplicationController
   Tmdb::Api.key("6ce86077881a441d243745e068c0f6dc")
   Tmdb::Api.language("ja") # 映画情報の表示の際の言語設定を日本語にできる
 
-
-
   def index
     @tmdbs = JSON.parse(Tmdb::Movie.upcoming.to_json)
   end
