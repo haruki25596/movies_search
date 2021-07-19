333ff0b043e29d75b18f28a876d8d52f258e0e8e
haruki25596 <gymnasticc.1950.softbank.jp@gmail.com>
2021-07-19 17:01:56 +0000

    レビューに対するコメントにkaminari追加

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -3,7 +3,7 @@ class ReviewCommentsController < ApplicationController
 
   def new
     @review_comment = ReviewComment.new
-    @review_comments = ReviewComment.where(review_id: params[:review_id])
+    @review_comments = ReviewComment.where(review_id: params[:review_id]).page(params[:page]).per(8)
     @review = Review.find(params[:review_id])
   end
 
